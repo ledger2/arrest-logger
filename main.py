a479229508b1c9d01628bd71579ccde65c0737ac
ledger2 <austinledger33@gmail.com>
2019-01-01 00:54:47 -0600

    Eliminated urllib in favor of requests library

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
-import urllib3
+import requests
 import configparser
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 root = "https://www8.iowa-city.org/icgov/apps/police/blotter.asp?date="
+pushing = False
 
+#Try to setup Discord webhook to push notifications
 def tryDiscord():
 	try:
-		import requests
 		import discord
 		from discord import Webhook, RequestsWebhookAdapter, File
 	except ImportError:
-		requests = None
 		discord = None 
-	if requests && discord:
+	if discord:
 		print("Discord integration loading...")
 		config = configparser.ConfigParser()
 		URL = config["DiscordIntegration"]["WebhookURL"]
-		hook = Webhook.from_url(URL, adapter=RequestsWebhookAdapter())
-		print("Webhook setup complete")
-	
+		if(URL != None):
+			hook = Webhook.from_url(URL, adapter=RequestsWebhookAdapter())
+			pushing = True
+			print("Webhook setup complete")
+		else:
+			print("Webhook URL not supplied in 'config.ini'!")
+
+#Fetch data on specified date (dateString MMDDYYYY)
 def query(dateString):
-	url = root+dateString
-	http = urllib3.PoolManager()
-	r = http.request("GET", url)
-	if(r.status == 200):
-		return r.data
+	get = {"date":dateString}
+	r = requests.get(root, params=get)
+	if(r.status_code == 200):
+		#Successful fetch, format and return data
+ 		return r.text
 	else:
-		print("Query failed. (Status: "+str(r.status)+")")
+		print("Query failed. (Status: "+str(r.status_code)+")")
+		
+def prettyPrintout(dict):
+	#dictionary formatting: {"name": [offenseDatetime, dateOfBirth, arrestLocation, incidentNum, arrested, chargesList]}
 
 tryDiscord()
 
-pageData = query(str(datetime.now().month)+str(datetime.now().day-1)+str(datetime.now().year))
+dateToFetch = datetime.now()-datetime.timedelta()
+newData = query(str())
 print("Fetched...\n")
-print(pageData)
+print(newData)
